eea21a558e33bfdee8114e390d0afd10e75d35c5
linuxfeifei <1649079787@qq.com>
2015-06-03 01:51:21 -0700

    c-cpp

--- final-test/c_cpp/my_stack.c
+++ final-test/c_cpp/my_stack.c
@@ -4,5 +4,36 @@
  * 但是记住g++才是编译C++的编译器
  *
  */
+#include <stdio.h>
+
+#include "include/my_stack.h"
+int a[10],i=0;
+int stack_is_full() 
+{
+ if(i==10) return 1;
+else if(i<10) return 0;
+}
+int stack_is_empty() 
+{
+ if(i<0) return 1;
+else if(i>=0) return 0;
+}
+void stack_push(int x)
+{
+  a[i]=x;
+  i++;
+}
+int stack_pop()
+{   
+   int x;
+i--;
+if(!stack_is_empty() )
+{
+x=a[i];
+return x;
+}
+} 
+      
+
 
 
